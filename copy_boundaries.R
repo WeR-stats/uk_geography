d6cdbb4b90b6ea0ff8b77ba27c81cf1e12bbc6ac
Luca Valnegri <l.valnegri@datamaps.co.uk>
2019-03-31 18:09:27 +0100

    first commit

---
+++ copy_boundaries.R
@@ -0,0 +1,17 @@
+###########################
+# UK GEOGRAPHY BOUNDARIES #
+###########################
+
+# load packages
+
+
+# set constants
+areas = c('OA', 'LSOA', 'MSOA', 'LAD',  'CTY',  'RGN',  'CTRY', 'WARD', 'CCG')
+data_path <- file.path(Sys.getenv('PUB_PATH'), 'boundaries', 'geography', 'uk', 'rds', 's20')
+
+# copy boundaries
+for(area in areas) system(paste0('cp ', data_path, '/', area, ' ./boundaries/'))
+    
+# clean env
+rm(list = ls())
+gc()
